3e4e05465a886f3e1c3daa2d83e6a942509dbe2b
Stringmander <nrusso@protonmail.com>
2020-10-07 12:51:50 -0400

    Add static method for html report for wins analysis

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,6 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsHtmlReport('Man United');
 
 summary.buildAndPrintReport(matchReader.matches);
